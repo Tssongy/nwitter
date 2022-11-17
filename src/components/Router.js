90d396d5e45985026430610c42be79ee5041d35e
Tssongy <taemine.song@gmail.com>
2022-11-17 20:48:30 +1100

    dummy commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,7 +2,7 @@ import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Profile from "../routes/Profile";
-import Navigation from "./Navigation";
+import Navigation from "./Navigation"
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
