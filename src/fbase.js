dbaf02503944d8aaee97043e468f62054f6ea0d5
Tssongy <taemine.song@gmail.com>
2022-11-20 22:36:48 +1100

    added GET and WRITE nweets with a firestore

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app;
 export const authService = getAuth(app);
+export const dbService = getFirestore();
+export default app;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { dbService } from "../fbase";
+import { collection, addDoc, getDocs, query } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id:doc.id
+            }
+            console.log(doc.data())
+            setNweets(prev => [nweetObj, ...prev])
+        })
+    }
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
     };
 
     const onChange = (event) => {
-        const value = event.target;
+        const { value } = event.target;
+        console.log(value)
         setNweet(value);
     }
+
+    useEffect(() => {
+        getNweets();
+    }, [])
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-            <input onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-            <input type="submit" value="Nweet" />
+                <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => 
+                    <div>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
