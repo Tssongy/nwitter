67a3cab2df355fd1fda8df023ba1264d269d476a
unknown <taemine.song@gmail.com>
2023-01-04 20:14:06 +1100

    updated UI for file uploads

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import { collection, addDoc, getDocs, query, orderBy, onSnapshot } from "firebas
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     
     const getNweets = async () => {
         const q = query(collection(dbService, "nweets"));
@@ -49,12 +50,35 @@ const Home = ({ userObj }) => {
             setNweets(nweetArr);
         });
     }, [])
-
+    const onFileChange = (event) => {
+        const { files } = event.target;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const { currentTarget: { result } } = finishedEvent;
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null);
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input 
+                    onChange={onChange} 
+                    value={nweet} 
+                    type="text" 
+                    placeholder="What's on your mind?" 
+                    maxLength={120} 
+                />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && 
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                }
             </form>
             <div>
                 {nweets.map((nweet) => 
