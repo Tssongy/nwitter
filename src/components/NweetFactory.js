063dd83aa4a744c6e498a643ac56ed51fa99e66d
unknown <taemine.song@gmail.com>
2023-01-20 22:22:07 +1100

    updated more styles

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -4,6 +4,8 @@ import { dbService } from "../fbase";
 import { v4 as uuidv4} from "uuid";
 import { storageService } from "../fbase";
 import { collection, addDoc } from "firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const NweetFactory = ({ userObj }) => {
     const [attachment, setAttachment] = useState("");
@@ -51,22 +53,45 @@ const NweetFactory = ({ userObj }) => {
     const onClearAttachment = () => setAttachment("");
 
     return (
-        <form onSubmit={onSubmit}>
-            <input 
-                onChange={onChange} 
-                value={nweet} 
-                type="text" 
-                placeholder="What's on your mind?" 
-                maxLength={120} 
+        <form onSubmit={onSubmit} className="factoryForm">
+            <div className="factoryInput__container">
+                <input 
+                    className="factoryInput__input"
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="&rarr;" className="factoryInput__arrow" />
+            </div>
+            <label for="attach-file" className="factoryInput__label">
+                <span>Add photos</span>
+                <FontAwesomeIcon icon={faPlus} />
+            </label>
+            <input
+                id="attach-file" 
+                type="file" 
+                accept="image/*" 
+                onChange={onFileChange} 
+                style={{
+                    opacity: 0,
+                }}
             />
-            <input type="file" accept="image/*" onChange={onFileChange} />
-            <input type="submit" value="Nweet" />
-            {attachment && 
-                <div>
-                    <img src={attachment} width="50px" height="50px" />
-                    <button onClick={onClearAttachment}>Clear</button>
+            {attachment && (
+                <div className="factoryForm__attachment">
+                    <img
+                        src={attachment}
+                        style={{
+                        backgroundImage: attachment,
+                        }}
+                    />
+                    <div className="factoryForm__clear" onClick={onClearAttachment}>
+                        <span>Remove</span>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </div>
                 </div>
-            }
+            )}
         </form>
     )
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@
 // );
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import "../src/style.css"
 
 import App from "./components/App";
 
